75dea1bea815e791890020d34265ba945548dab5
root <m-ohammed45>
2024-01-10 01:12:53 -0800

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,21 @@
+def square_matrix_simple(matrix=[]):
+    result_matrix =[]
+
+    for row in matrix:
+    
+        squared_row = list(map(lambda x: x**2, row))
+        
+        result_matrix.append(squared_row)
+    
+    return result_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
+
