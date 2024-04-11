cc9918a4e73573ab3e0a91258556b27b86338bf3
m-ohammed45 <mohammedfarahat30@gmail.com>
2024-04-11 02:37:18 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,8 @@ const [,, arg1, arg2] = process.argv;
 
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
+} else if (arg1 && !arg2) {
+  console.log(`${arg1} is undefined`);
 } else {
-  console.log('Two arguments are required');
+  console.log('undefined is undefined');
 }
