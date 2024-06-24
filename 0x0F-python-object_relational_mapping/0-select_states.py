0d08662bea1b5c370e7df4e69c8d80d1cb26964c
m-ohammed45 <mohammedfarahat30@gmail.com>
2024-06-24 00:40:06 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,35 +5,28 @@ Script that connects to a MySQL database and prints all states sorted by id.
 Usage:
     ./0-select_states.py <username> <password> <database>
 """
+
+import MySQLdb
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
-
-class State(Base):
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(256), nullable=False)
+def list_states(username, password, db_name):
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
+        print("Usage: python list_states.py <username> <password> <db_name>")
         sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print(f"({state.id}, '{state.name}')")
-    session.close()
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, db_name)
 
